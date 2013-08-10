48cba025922f67b2b0bc3a18a56915f960b5ca06
rboling91@gmail.com <rboling91@gmail.com>
2013-08-10 10:41:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreedomhackApp::Application.config.secret_token = '35bebc871392c396e2752bd68e95b564b53a42595d6d805f7db3a1d38b5b80684afbb2fcc5deb0b6ac0b18f1c77bd7e3e33d6c66e7b216e89ed6af6100063a78'
