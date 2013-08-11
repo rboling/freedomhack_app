2229d0ff55de237955d57240af7a1bb64e0bbdce
rboling91@gmail.com <rboling91@gmail.com>
2013-08-11 08:58:28 -0400

    some new files

--- app/models/text_message.rb
+++ app/models/text_message.rb
@@ -48,6 +48,8 @@ class TextMessage < ActiveRecord::Base
   def self.secret_code_find(the_user, the_code)
   	code_found = false
   	refined_user_array = User.all.select{|x| x.id != the_user.id}
+  	puts "\n\nREFINED ARRAY\n\n"
+  	puts refined_user_array
   	refined_user_array.each do |user|
   	  text_messages = user.text_messages
   	  text_messages.each do |message|
